d67ad18b3c74679f1d4d81df8ae084b2f66dcc92
xcorpion <umarmughal255372@gmail.com>
2024-12-27 15:41:44 +0500

    Migrated Model

---
+++ prisma/migrations/20241226193847_added_user_schema/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "image" TEXT,
+    "oauth_id" TEXT NOT NULL,
+    "coins" INTEGER NOT NULL DEFAULT 50,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
